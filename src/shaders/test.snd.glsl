0838e225c4645bb89a0a4fd24414d45cbb8c1ed7
0b5vr <0b5vr@0b5vr.com>
2023-10-08 21:59:08 +0900

    size (sound): 地道な最適化
    - 定数 `BPS` ・ `B2T` ・ `S2T` とかがそれぞれトークン消費していたので、最小限に抑えた
    - 頻繁に使用する `float t` を `main` のスコープに
    - 人の手による温かみのあるExplicit Inlining
    - `exp( -k * t )` を全部 `exp( -t * k )` にしたらめちゃくちゃサイズ小さくなってきも
    - ついでにクラッシュシンバル追加
    
    5,341 bytes (- 110 bytes)

--- src/shaders/test.snd.glsl
+++ src/shaders/test.snd.glsl
@@ -4,13 +4,10 @@ const float PI = acos( -1.0 );
 const float TAU = PI * 2.0;
 const float GOLD = PI * (3.0 - sqrt(5.0));// 2.39996...
 
-const float BPS = 2.1;
-const float B2T = 1.0 / BPS;
-const float S2T = 0.25 * B2T;
 const float SAMPLES_PER_SEC = 48000.0;
 
-int SAMPLES_PER_STEP = int( SAMPLES_PER_SEC / BPS / 4.0 );
-int SAMPLES_PER_BEAT = 4 * SAMPLES_PER_STEP;
+const float STEP2TIME = 60.0 / 128.0 / 4.0;
+const int SAMPLES_PER_STEP = 5625;
 
 uvec3 pcg3d(uvec3 v)
 {
@@ -113,8 +110,8 @@ float CHORDS[] = float[](
 
 void main() {
   int frame = int( gl_GlobalInvocationID.x ) + waveOutPosition;
-  vec4 time = vec4( ( frame ) % ( SAMPLES_PER_BEAT * ivec4( 1, 4, 64, 65536 ) ) ) / SAMPLES_PER_SEC;
-  vec4 beats = time * BPS;
+  vec4 time = vec4( ( frame ) % ( SAMPLES_PER_STEP * ivec4( 4, 16, 256, 65536 ) ) ) / SAMPLES_PER_SEC;
+  vec4 beats = time / 4.0 / STEP2TIME;
 
   // return float( max( 0, frame + SAMPLES_PER_STEP * offset ) % ( SAMPLES_PER_STEP * every ) ) / SAMPLES_PER_SEC;
 
@@ -126,6 +123,7 @@ void main() {
 
   bool i_tenkaiKickActive = beats.w >= 64.0;
   bool i_tenkaiBassActive = beats.w >= 32.0;
+  bool i_tenkaiCrashActive = beats.w >= 32.0;
   bool i_tenkaiHihatActive = ( beats.w >= 32.0 && beats.w < 128.0 ) || beats.w >= 192.0;
   bool i_tenkaiClapActive = ( beats.w >= 64.0 && beats.w < 128.0 ) || beats.w >= 256.0;
   bool i_tenkaiPercActive = ( beats.w >= 64.0 && beats.w < 128.0 ) || beats.w >= 192.0;
@@ -133,36 +131,34 @@ void main() {
   float i_pluckFilterEnv = smoothstep( 256.0, 192.0, beats.w );
   // -- tenkai end ---------------------------------------------------------------------------------
 
-  int prog = beats.w >= 128.0
-    ? ( int( time.w / 8.0 * BPS ) * 8 % 32 )
-    : 0;
+  float t;
+
+  int prog = max( 0, frame / SAMPLES_PER_STEP - TENKAI_PROG_STEP ) / 32 % 4 * 8;
 
   vec2 dest = vec2( 0.0 );
   float sidechain = 1.0;
 
   if ( i_tenkaiKickActive ) { // kick
-    float t = float( frame % SAMPLES_PER_BEAT ) / SAMPLES_PER_SEC;
-    float q = B2T - t;
-    sidechain = 0.2 + 0.8 * smoothstep( 0.0, 0.4, t ) * smoothstep( 0.0, 0.001, q );
+    t = float( frame % ( 4 * SAMPLES_PER_STEP ) ) / SAMPLES_PER_SEC;
+    float i_q = 4.0 * STEP2TIME - t;
+    sidechain = 0.2 + 0.8 * smoothstep( 0.0, 0.4, t ) * smoothstep( 0.0, 0.001, i_q );
 
-    float env = smoothstep( 0.0, 0.001, q ) * smoothstep( 0.3, 0.1, t );
+    float i_env = smoothstep( 0.0, 0.001, i_q ) * smoothstep( 0.3, 0.1, t );
 
-    float wave = sin(
+    float i_wave = sin(
       270.0 * t
-      - 20.0 * exp( -20.0 * t )
-      - 15.0 * exp( -80.0 * t )
-      - 10.0 * exp( -500.0 * t )
+      - 20.0 * exp( -t * 20.0 )
+      - 15.0 * exp( -t * 80.0 )
+      - 10.0 * exp( -t * 500.0 )
     );
-    dest += 0.6 * tanh( 2.0 * env * wave );
+    dest += 0.6 * tanh( 2.0 * i_env * i_wave );
   }
 
   if ( i_tenkaiBassActive ) { // bass
-    float t = float( frame % SAMPLES_PER_STEP ) / SAMPLES_PER_SEC;
-    float q = S2T - t;
+    t = float( frame % SAMPLES_PER_STEP ) / SAMPLES_PER_SEC;
+    float i_q = STEP2TIME - t;
 
-    float env = smoothstep( 0.0, 0.001, t ) * smoothstep( 0.0, 0.001, q );
-
-    float k = 0.55 - 0.4 * exp( -5.0 * t );
+    float env = smoothstep( 0.0, 0.001, t ) * smoothstep( 0.0, 0.001, i_q );
 
     {
       float phase = t * i_p2f( 30.0 + CHORDS[ prog ] );
@@ -171,39 +167,52 @@ void main() {
 
     for ( int i = 0; i < 8; i ++ ) {
       vec3 dice = pcg33( vec3( i + 51 ) );
-      vec2 dicei = i_boxMuller( dice.xy );
-      float phase = t * i_p2f( 30.0 + CHORDS[ prog ] + 0.2 * dicei.x ) + dice.z;
+      vec2 i_dicei = i_boxMuller( dice.xy );
+      float phase = t * i_p2f( 30.0 + CHORDS[ prog ] + 0.2 * i_dicei.x ) + dice.z;
 
-      float wave = tanh( 3.0 * i_cheapFilterSaw( phase, k ) );
-      dest += 0.1 * sidechain * tanh( 2.0 * env * wave );
+      float i_k = 0.55 - 0.4 * exp( -t * 5.0 );
+      float i_wave = tanh( 3.0 * i_cheapFilterSaw( phase, i_k ) );
+      dest += 0.1 * sidechain * tanh( 2.0 * env * i_wave );
     }
   }
 
-  if ( i_tenkaiHihatActive ) { // hihat
-    float st = floor( frame / SAMPLES_PER_STEP % 16 );
-    float t = float( frame % SAMPLES_PER_STEP ) / SAMPLES_PER_SEC;
-    float q = S2T - t;
+  if ( i_tenkaiCrashActive ) { // crash
+    t = float( frame % ( 256 * SAMPLES_PER_STEP ) ) / SAMPLES_PER_SEC;
 
-    float decay = exp2( 5.0 + fract( 0.8 + 0.631 * vec3( st ) ).x );
-    float env = smoothstep( 0.0, 0.001, q ) * exp( -decay * t );
+    float env = exp( -t * 10.0 ) + exp( -t ); // don't inline me!
 
     // shotgun
+    t *= 28000.0;
     vec2 wave = vec2( 0.0 );
     for ( int i = 0; i < 64; i ++ ) {
       vec3 dice = pcg33( vec3( i ) );
-      wave += vec2( sin( 37700.0 * t * exp2( 2.0 * dice.x ) ) ) * rot( TAU * dice.y ) / 64.0;
+      wave += vec2( sin( t * exp2( 2.0 * dice.x ) ) ) * rot( TAU * dice.y ) / 64.0;
+    }
+    dest += 0.2 * sidechain * tanh( 2.0 * env * wave );
+  }
+
+  if ( i_tenkaiHihatActive ) { // hihat
+    t = float( frame % SAMPLES_PER_STEP ) / SAMPLES_PER_SEC;
+    float i_q = STEP2TIME - t;
+    float i_st = floor( frame / SAMPLES_PER_STEP % 16 );
+
+    float i_decay = exp2( 5.0 + fract( 0.8 + 0.631 * vec3( i_st ) ).x );
+    float env = smoothstep( 0.0, 0.001, i_q ) * exp( -t * i_decay ); // don't inline me!
+
+    // shotgun
+    t *= 38000.0;
+    vec2 wave = vec2( 0.0 );
+    for ( int i = 0; i < 64; i ++ ) {
+      vec3 dice = pcg33( vec3( i ) );
+      wave += vec2( sin( t * exp2( 2.0 * dice.x ) ) ) * rot( TAU * dice.y ) / 64.0;
     }
     dest += 0.3 * sidechain * tanh( 2.0 * env * wave );
   }
 
   if ( i_tenkaiClapActive ) { // clap
-    float t = mod( time.y - B2T, 2.0 * B2T );
+    t = float( ( frame + 4 * SAMPLES_PER_STEP ) % ( 8 * SAMPLES_PER_STEP ) ) / SAMPLES_PER_SEC;
 
-    float i_env = mix(
-      exp( -80.0 * mod( t, 0.02 ) ),
-      exp( -20.0 * t ),
-      smoothstep( 0.02, 0.04, t )
-    );
+    float i_env = exp( -t * 16.0 );
 
     vec2 i_wave = fbm32( vec2( 4.0 * orbit( 1000.0 * t ) + 480.0 * t ) ).xy - 0.5;
 
@@ -211,25 +220,24 @@ void main() {
   }
 
   if ( i_tenkaiPercActive ) { // perc
-    for ( int i = 0; i < 16; i ++ ) {
+    for ( int i = 0; i < 32; i ++ ) {
       vec3 dice = pcg33( vec3( i ) + 4.995 );
       vec3 dice2 = pcg33( dice );
 
-      float t = time.z;
-      t = mod(
-        mod( t, 8.0 * B2T ) - 0.25 * B2T * float( i ),
-        0.25 * B2T * floor( 3.0 + 14.0 * dice.x )
-      );
+      int i_every = 3 + int( 14.0 * dice.x );
+      t = float( ( frame % ( 32 * SAMPLES_PER_STEP ) + i * SAMPLES_PER_STEP ) % ( i_every * SAMPLES_PER_STEP ) ) / SAMPLES_PER_SEC;
 
-      float env = exp( -exp2( 5.0 + 3.0 * dice.y ) * t );
+      float i_env = exp( -t * exp2( 5.0 + 3.0 * dice.y ) );
 
-      float freq = exp2( 8.0 + 8.0 * dice2.x );
-      vec2 wave = fbm32( vec2(
-        exp2( -2.0 + 4.0 * dice2.y ) * orbit( freq * t )
-        + exp2( -2.0 + 5.0 * dice2.z ) * freq * t
+      float i_freq = exp2( 8.0 + 8.0 * dice2.x );
+      float phase = i_freq * t;
+
+      vec2 i_wave = fbm32( vec2(
+        exp2( -2.0 + 4.0 * dice2.y ) * orbit( phase )
+        + exp2( -2.0 + 5.0 * dice2.z ) * phase
       ) ).xy - 0.5;
 
-      dest += 0.08 * sidechain * tanh( 6.0 * env * wave );
+      dest += 0.08 * sidechain * tanh( 6.0 * i_env * i_wave );
     }
   }
 
@@ -242,47 +250,41 @@ void main() {
     int i_frameStart = frame - i_frameNote;
     int i_prog = max( 0, i_frameStart / SAMPLES_PER_STEP - TENKAI_PROG_STEP ) / 32 % 4 * 8;
 
-    float t = float( i_frameNote ) / SAMPLES_PER_SEC;
-    float q = 0.75 * B2T - t;
-    float env = smoothstep( 0.0, 0.001, t ) * smoothstep( 0.0, 0.001, q ) * exp( -3.0 * t );
+    t = float( i_frameNote ) / SAMPLES_PER_SEC;
+    float i_q = 3.0 * STEP2TIME - t;
+    float i_env = smoothstep( 0.0, 0.001, t ) * smoothstep( 0.0, 0.001, i_q ) * exp( -t * 3.0 );
 
     int chordn = 4 + clamp( ( i_frameStart / SAMPLES_PER_STEP - TENKAI_CHORD_START_STEP ) / TENKAI_CHORD_LENGTH_STEP, 0, 4 );
     float freq = i_p2f( 54.0 + CHORDS[ i % chordn + i_prog ] + 0.1 * clamp( dicen.x, -1.0, 1.0 ) );
     float phase = freq * time.z + dice.x;
 
-    { // pluck
-      float k = 0.15 * i_pluckFilterEnv + 2.0 * t;
-      vec2 wave = vec2( i_cheapFilterSaw( phase, k ) );
-      dest += 0.03 * sidechain * env * wave * rot( float( i ) );
-    }
+    float i_k = 0.15 * i_pluckFilterEnv + 2.0 * t;
+    vec2 i_wave = vec2( i_cheapFilterSaw( phase, i_k ) );
+    dest += 0.03 * sidechain * i_env * i_wave * rot( float( i ) );
   }
 
   { // arp
-    vec2 sum=vec2(0);
-
     for ( int i = 0; i < 64; i ++ ) {
-      float t = float( max( 0, frame - ( i + TENKAI_ARP_START_STEP ) * SAMPLES_PER_STEP ) % ( 64 * SAMPLES_PER_STEP ) ) / SAMPLES_PER_SEC;
+      t = float( max( 0, frame - ( i + TENKAI_ARP_START_STEP ) * SAMPLES_PER_STEP ) % ( 64 * SAMPLES_PER_STEP ) ) / SAMPLES_PER_SEC;
 
-      float fi = float( i );
-      float t2f = mod( t, 0.75 * B2T );
-      float t2i = floor( t / ( 0.75 * B2T ) );
-      float q = 0.75 * B2T - t2f;
-      float env = smoothstep( 0.0, 0.002, t2f ) * smoothstep( 0.0, 0.01, q ) * exp(-12.*t2f) * exp(-t2i);
+      float i_fi = float( i );
+      float t2f = mod( t, 3.0 * STEP2TIME );
+      float i_t2i = floor( t / ( 3.0 * STEP2TIME ) );
+      float i_q = 3.0 * STEP2TIME - t2f;
+      float i_env = smoothstep( 0.0, 0.002, t2f ) * smoothstep( 0.0, 0.01, i_q ) * exp( -t2f * 12.0 ) * exp( -i_t2i );
 
-      int arpseed = int( 21.0 * fract( 0.825 * fi ) );
-      float note = 42.0 + CHORDS[ arpseed % 8 + prog ] + 12.0 * float( arpseed / 8 );
-      float freq = i_p2f( note );
-      vec2 phase = t * freq + pcg33( vec3( fi ) ).xy;
-      phase += 0.4 * fbm32( vec2( 0.1 * phase.x ) ).xy;
+      int arpseed = int( 21.0 * fract( 0.825 * i_fi ) );
+      float i_note = 42.0 + CHORDS[ arpseed % 8 + prog ] + 12.0 * float( arpseed / 8 );
+      float freq = i_p2f( i_note );
+      vec2 phase = (
+        t * freq
+        + pcg33( vec3( i_fi ) ).xy
+        + 0.4 * fbm32( vec2( 0.1 * t * freq ) ).xy
+      );
 
-      vec2 wave = vec2( sin(
-        TAU * phase
-      ) );
-
-      sum+=2.*env*wave*rot(time.w);
+      vec2 i_wave = vec2( sin( TAU * phase ) );
+      dest += 0.2 * sidechain * i_env * i_wave * rot( time.w );
     }
-
-    dest += 0.1 * sidechain * sum;
   }
 
   waveOutSamples[ frame ] = dest;
