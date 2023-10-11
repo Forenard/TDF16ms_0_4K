bff562b0d2f7af80e0457a0a371add4de4ee7403
Forenard <forenard2002@gmail.com>
2023-10-12 01:12:34 +0900

    だいたい展開4,490(-13)

--- src/shaders/test.gfx.glsl
+++ src/shaders/test.gfx.glsl
@@ -20,8 +20,8 @@ layout(location = 2) uniform float time;
 layout(location = 3) uniform vec2 resolution;
 #endif
 
-out vec4 outColor0;
-// layout(location = 0) out vec4 outColor0;
+// out vec4 outColor0;
+layout(location = 0) out vec4 outColor0;
 // layout(location = 1) out vec4 outColor1;
 
 // 
@@ -62,16 +62,15 @@ const vec3 k12000 = vec3(191, 211, 255) / 255.0;
 #define opRepLim(p,c,l) ((p)-(c)*clamp(round((p)/(c)),-(l),(l)))
 #define opRepLimID(p,c,l) (clamp(round((p)/(c)),-(l),(l))+(l))
 
-
 vec2 orbit(float t)
 {
-  return vec2(cos(t), sin(t));
+    return vec2(cos(t), sin(t));
 }
 
 mat2 rot(float x)
 {
-  vec2 v = orbit(x);
-  return mat2(v.x, v.y, -v.y, v.x);
+    vec2 v = orbit(x);
+    return mat2(v.x, v.y, -v.y, v.x);
 }
 
 bool tl(float intime, float outtime, out float lt)
@@ -146,7 +145,7 @@ float pow5(float x)
     return (x * x) * (x * x) * x;
 }
 
-vec3 Microfacet_BRDF(vec3 albedo, float metallic, float paramRoughness, vec3 L, vec3 V, vec3 N, bool isSecondary)
+vec3 Microfacet_BRDF(vec3 albedo, float metallic, float paramRoughness, vec3 L, vec3 V, vec3 N)
 {
     // i think 0.5
     const float reflectance = 0.5;
@@ -175,7 +174,7 @@ vec3 Microfacet_BRDF(vec3 albedo, float metallic, float paramRoughness, vec3 L, 
     // Calc F
     // vec3 F_spec = f0 + (1.0 - f0) * pow(1.0 - LoH, 5.0);
     vec3 F_spec = f0 + (1.0 - f0) * pow5(1.0 - LoH);
-    vec3 Fr = (isSecondary ? 1.0 : D_spec * V_spec) * F_spec;
+    vec3 Fr =  D_spec * V_spec * F_spec;
 
     // Calc diffuse
     float f90 = 0.5 + 2.0 * roughness * LoH * LoH;
@@ -196,15 +195,14 @@ vec3 Microfacet_BRDF(vec3 albedo, float metallic, float paramRoughness, vec3 L, 
 // ..%%%%...%%%%%...%%.....
 // ........................
 // 
-
 // https://mercury.sexy/hg_sdf/
-float i_fOpUnionRound(float a, float b, float r)
+float fOpUnionRound(float a, float b, float r)
 {
     return min(a, b);
     // vec2 u = max(vec2(r - a, r - b), vec2(0));
     // return max(r, min(a, b)) - length(u);
 }
-float i_fOpDifferenceRound(float a, float b, float r)
+float fOpDifferenceRound(float a, float b, float r)
 {
     return max(a, -b);
     // vec2 u = max(vec2(r + a, r - b), vec2(0));
@@ -268,22 +266,22 @@ float sdf(vec3 p)
     // ベースの壁
     float hd = -p.z;
     // 部屋のあな
-    hd = i_fOpDifferenceRound(hd, sdBox(p, vec3(RoomSize - 0.2, 4.0)), 0.01);
+    hd = fOpDifferenceRound(hd, sdBox(p, vec3(RoomSize - 0.2, 4.0)), 0.01);
     // 窓枠
     tp = p - vec3(0, 0, 0.75);
     tp.x = abs(tp.x);
     const vec2 rsize = RoomSize - 0.2;
-    hd = i_fOpUnionRound(hd, sdBox(tp, vec3(rsize, 0.05)), 0.01);
+    hd = fOpUnionRound(hd, sdBox(tp, vec3(rsize, 0.05)), 0.01);
     const vec3 msize = vec3(rsize.x * 0.25 - 0.125, rsize.y * 0.75, 0.1);
-    hd = i_fOpDifferenceRound(hd, sdBox(tp - vec3(msize.x * 0.5 + 0.05, -msize.y * 0.1, 0), msize), 0.01);
+    hd = fOpDifferenceRound(hd, sdBox(tp - vec3(msize.x * 0.5 + 0.05, -msize.y * 0.1, 0), msize), 0.01);
     const vec3 msize2 = vec3(msize.x, msize.y * 0.5, msize.z);
-    hd = i_fOpDifferenceRound(hd, sdBox(tp - vec3(msize2.x * 1.5 + 0.15, msize2.y * 0.3, 0), msize2), 0.01);
+    hd = fOpDifferenceRound(hd, sdBox(tp - vec3(msize2.x * 1.5 + 0.15, msize2.y * 0.3, 0), msize2), 0.01);
     // 腰壁
     hd = min(hd, -0.01 + sdBox(p - vec3(0, -RoomSize.y * 0.25, 0.1), vec3(RoomSize.x - 0.2, RoomSize.y * 0.3, 0.025)));
     // 腰壁の穴
     tp = p - vec3(0, -RoomSize.y * 0.25, 0.1);
     tp.x = opRepLim(tp.x, 0.2, 6);
-    hd = i_fOpDifferenceRound(hd, sdBox(tp, vec3(0.06, RoomSize.y * 0.25, 0.05)), 0.01);
+    hd = fOpDifferenceRound(hd, sdBox(tp, vec3(0.06, RoomSize.y * 0.25, 0.05)), 0.01);
     // 手すり
     hd = min(hd, -0.01 + sdBox(p - vec3(0, -RoomSize.y * 0.1 - 0.025, 0), vec3(RoomSize.x - 0.2, 0.05, 0.2)));
 
@@ -291,7 +289,7 @@ float sdf(vec3 p)
     const vec3 size = vec3(0.4, 0.28, 0.1);
     td = -0.01 + sdBox(p - vec3(1.1, -0.77, -0.02), size);
     tp = p - vec3(1.1, -0.77, -0.02) - vec3(-0.06, 0, -0.1);
-    td = i_fOpDifferenceRound(td, sdBox(tp,vec3(0,0,0.1))-0.1, 0.005);
+    td = fOpDifferenceRound(td, sdBox(tp,vec3(0,0,0.1))-0.1, 0.005);
     tp.z -= 0.05;
     td = min(td, length(tp)-0.015);
     // ひだひだ
@@ -299,7 +297,7 @@ float sdf(vec3 p)
     float a = atan(tp.y, tp.x);
     a = mod(a, div * 2.0) - div;
     tp.xy = orbit(a) * length(tp.xy);
-    td = i_fOpUnionRound(td, sdBox(tp,vec3(0.2,0,0))-0.002, 0.005);
+    td = fOpUnionRound(td, sdBox(tp,vec3(0.2,0,0))-0.002, 0.005);
     hd = min(hd, td);
     // 部屋限定
     hd += isroom;
@@ -406,13 +404,10 @@ const vec3 DirectionalLight = normalize(vec3(1, 1, -1));
 
 vec3 sky(vec3 rd)
 {
-    vec3 gcol = vec3(0.01);
-    float theta = atan(rd.x, -rd.z);
-    float phi = acos(rd.y);
-    vec2 th = vec2(theta, phi * 2.0) / TAU * 20.0 + vec2(0.05, 0.01) * Time;
+    vec2 th = vec2(atan(rd.x, -rd.z), acos(rd.y) * 2.0) / TAU * 20.0 + vec2(0.05, 0.01) * Time;
     vec3 scol = vec3(mix(0.1, 0.5, fbm32(th).x));
     float up = saturate((rd.y + 0.5) * 0.5);
-    vec3 col = mix(gcol, scol, up);
+    vec3 col = mix(vec3(0.01), scol, up);
     // sun
     float sun = saturate(dot(rd, DirectionalLight));
     sun = smoothstep(0.8, 1.0, sun);
@@ -422,97 +417,47 @@ vec3 sky(vec3 rd)
     return col;
 }
 
-vec3 shading(inout vec3 P, vec3 V, vec3 N)
-{
-    // triplanar
-    vec3 tN = sign(N) * abs(N);
-    tN = tN / dot(vec3(1), tN);
-    vec2 uv = tN.x * P.zy + tN.y * P.xz + tN.z * P.xy;
-
-    // identify
-    vec3 RP = P;
-    vec2 ip = opRoomRep(RP);
-    bool isfloorB = fract(0.5 * ip.x / RoomSize.x) < 0.5;
-    vec3 h3 = pcg33(vec3(0.1, ip));
-
-    // get mat
-    float type = MAT_PBR;
-    vec3 albedo = vec3(1);
-    float roughness = 0.5;
-    float metallic = 0.5;
-    // ポイントライトの色
-    vec3 plcol = (step(h3.y, 0.9) * 0.8 + 0.2) * mix(k2000, k12000, h3.x);
-    if(MatID == 0)
-    {
-        // Mat:Concrete
-        vec3 fbm = fbm32(uv * 3.0 * vec2(3, 1));// gravity ydown
-        vec3 fbm2 = fbm32(uv * 96.0 * vec2(2, 1));
-        albedo = vec3(saturate(1.3 * mix(0.6, 1.0, fbm.y) * mix(0.8, 1.0, pow(fbm2.x, 3.0))));
-        roughness = mix(0.5, 1.0, pow(fbm.y, 3.0));
-        metallic = 0.01;
-        N = normalize(N + (fbm * 2.0 - 1.0) * 0.05);
-    }
-    else if(MatID == 1)
-    {
-        // Mat:カーテン
-        float h = h3.x;
-        albedo = (h < 0.7 ? vec3(0.8, 0.7, 0.6) : (h < 0.8 ? vec3(0.8, 0.2, 0.2) : (h < 0.9 ? vec3(0.8, 0.6, 0.3) : vec3(0.5, 0.7, 0.8))));
-        roughness = 0.99;
-        metallic = 0.01;
-    }
-    else
-    {
-        // Mat:ライト
-        type = MAT_UNLIT;
-        albedo = plcol;
-    }
-
-    // avoid self-intersection
-    P += N * DistMin * 2.0;
-    // directional shadow
-    vec3 _rp;
-    vec2 sh = march(DirectionalLight, P, _rp);
-    float visible = sh.y;
-
-    // primary shading
-    vec3 col = vec3(0);
-    // directional light
-    col += visible * Microfacet_BRDF(albedo, metallic, roughness, DirectionalLight, V, N, false) * k12000;
-    // point light
-    vec3 L = (isfloorB ? vec3(-RoomSize * 0.5 + 0.1, 2.9) : vec3(sign(h3.x - 0.5), -0.06, 0)) - RP;
-    float l = length(L);
-    L /= l;
-    float d = max(0.0, l);
-    vec3 lcol = plcol * pow(1.0 / (1.0 + d), 2.0);
-    col += Microfacet_BRDF(albedo, metallic, roughness, L, V, N, false) * lcol;
-
-    // ao
-    col *= sqrt(saturate(sdf(P + N * 0.05) / 0.05));
-    return mix(col, albedo, type);
-}
-
 // 
-// .%%%%%....%%%%....%%%%...%%%%%%.
-// .%%..%%..%%..%%..%%..%%....%%...
-// .%%%%%...%%..%%..%%..%%....%%...
-// .%%..%%..%%..%%..%%..%%....%%...
-// .%%..%%...%%%%....%%%%.....%%...
-// ................................
+// .%%...%%...%%%%...%%%%%%..%%..%%.
+// .%%%.%%%..%%..%%....%%....%%%.%%.
+// .%%.%.%%..%%%%%%....%%....%%.%%%.
+// .%%...%%..%%..%%....%%....%%..%%.
+// .%%...%%..%%..%%..%%%%%%..%%..%%.
+// .................................
 // 
 
-const vec2 FC = gl_FragCoord.xy;
-const vec2 RES = resolution.xy;
-const vec2 ASP = RES / min(RES.x, RES.y);
-vec2 FocalUV = vec2(0.5);
-
-void getRORD(out vec3 ro, out vec3 rd, out vec3 dir, vec2 suv)
+void main()
 {
+    // 
+    // ..%%%%...%%%%%%..%%%%%%..%%%%%%..%%%%%%..%%..%%...%%%%..
+    // .%%......%%........%%......%%......%%....%%%.%%..%%.....
+    // ..%%%%...%%%%......%%......%%......%%....%%.%%%..%%.%%%.
+    // .....%%..%%........%%......%%......%%....%%..%%..%%..%%.
+    // ..%%%%...%%%%%%....%%......%%....%%%%%%..%%..%%...%%%%..
+    // ........................................................
+    // 
+    // Set Time
+    Time = time;
+    // Get UVs
+    const vec2 fc = gl_FragCoord.xy, res = resolution.xy, asp = res / min(res.x, res.y);
+    const vec2 uv = fc / res;
+    // TAA
+    const vec3 h3 = pcg33(vec3(fc, Time));
+    const vec2 suv = (uv - 0.5 + ((h3.xy - 0.5) * 0.25 / res)) * 2.0 * asp;
+
+    // 
+    // ..%%%%....%%%%...%%...%%..%%%%%%..%%%%%....%%%%..
+    // .%%..%%..%%..%%..%%%.%%%..%%......%%..%%..%%..%%.
+    // .%%......%%%%%%..%%.%.%%..%%%%....%%%%%...%%%%%%.
+    // .%%..%%..%%..%%..%%...%%..%%......%%..%%..%%..%%.
+    // ..%%%%...%%..%%..%%...%%..%%%%%%..%%..%%..%%..%%.
+    // .................................................
+    // 
     // Parameter
     float fov = 60.0;
     float fisheye = 0.0;
-
-    ro = vec3(1);
-    dir = vec3(0, 0, 1);
+    vec3 ro = vec3(1), dir = vec3(0, 0, -1), rd;
+    // シーケンス
     float lt = 0.0;
     // Time = 46.28;
     if(tl(0.0, 15.0, lt))
@@ -561,86 +506,130 @@ void getRORD(out vec3 ro, out vec3 rd, out vec3 dir, vec2 suv)
         dir = normalize(vec3(0, 1, mix(0.0, 1.0, lt)));
         dir.xz *= rot(PI * lt);
     }
-
-    vec3 tebure = (fbm32(vec2(-4.2, Time * 0.1)) * 2.0 - 1.0) * 0.05;
+    // ADD 手振れ
+    const vec3 tebure = (fbm32(vec2(-4.2, Time * 0.1)) * 2.0 - 1.0) * 0.05;
     ro += tebure;
-
-    vec3 N = vec3(0, 1, 0);
-    vec3 B = normalize(cross(N, dir));
-    N = normalize(cross(dir, B));
-    mat3 bnt = mat3(B, N, dir);
-    float zf = 1.0 / tan(fov * PI / 360.0);
-    zf -= zf * length(suv) * fisheye;
-
+    // rdを計算
+    const vec3 B = normalize(cross(vec3(0, 1, 0), dir));
+    const mat3 bnt = mat3(B, normalize(cross(dir, B)), dir);
+    const float zf = 1.0 / tan(fov * PI / 360.0) * (1.0 - length(suv) * fisheye);
     rd = normalize(bnt * vec3(suv, zf));
-}
 
-vec4 tracer(vec3 rd, vec3 ro)
-{
-    vec3 rp;
-    if(march(rd, ro, rp).x < 0.5)
-    {
-        return vec4(sky(rd), LenMax);
-    }
-    float depth = length(rp - ro);
+    // 
+    // .%%%%%....%%%%...%%..%%..........%%%%%%..%%%%%....%%%%....%%%%...%%%%%%.
+    // .%%..%%..%%..%%...%%%%.............%%....%%..%%..%%..%%..%%..%%..%%.....
+    // .%%%%%...%%%%%%....%%..............%%....%%%%%...%%%%%%..%%......%%%%...
+    // .%%..%%..%%..%%....%%..............%%....%%..%%..%%..%%..%%..%%..%%.....
+    // .%%..%%..%%..%%....%%..............%%....%%..%%..%%..%%...%%%%...%%%%%%.
+    // ........................................................................
+    // 
+    // Trace
+    vec3 P;
+    vec2 hit = march(rd, ro, P);
+    float depth = length(P - ro);
     const float h = NormalEPS;
     const vec2 k = vec2(1, -1);
     int mid = MatID;
-    vec3 N = normalize(k.xyy * sdf(rp + k.xyy * h) + k.yyx * sdf(rp + k.yyx * h) + k.yxy * sdf(rp + k.yxy * h) + k.xxx * sdf(rp + k.xxx * h));
+    vec3 N = normalize(k.xyy * sdf(P + k.xyy * h) + k.yyx * sdf(P + k.yyx * h) + k.yxy * sdf(P + k.yxy * h) + k.xxx * sdf(P + k.xxx * h));
     MatID = mid;
-    vec3 col = shading(rp, -rd, N);
-    return vec4(col, depth);
-}
 
-// 
-// .%%...%%...%%%%...%%%%%%..%%..%%.
-// .%%%.%%%..%%..%%....%%....%%%.%%.
-// .%%.%.%%..%%%%%%....%%....%%.%%%.
-// .%%...%%..%%..%%....%%....%%..%%.
-// .%%...%%..%%..%%..%%%%%%..%%..%%.
-// .................................
-// 
+    // 
+    // ..%%%%...%%..%%...%%%%...%%%%%...%%%%%%..%%..%%...%%%%..
+    // .%%......%%..%%..%%..%%..%%..%%....%%....%%%.%%..%%.....
+    // ..%%%%...%%%%%%..%%%%%%..%%..%%....%%....%%.%%%..%%.%%%.
+    // .....%%..%%..%%..%%..%%..%%..%%....%%....%%..%%..%%..%%.
+    // ..%%%%...%%..%%..%%..%%..%%%%%...%%%%%%..%%..%%...%%%%..
+    // ........................................................
+    // 
+    // triplanar
+    vec3 tN = sign(N) * abs(N);
+    tN = tN / dot(vec3(1), tN);
+    vec2 tuv = tN.x * P.zy + tN.y * P.xz + tN.z * P.xy;
+    // identify
+    vec3 RP = P;
+    vec2 ip = opRoomRep(RP);
+    bool isfloorB = fract(0.5 * ip.x / RoomSize.x) < 0.5;
+    vec3 hash =  pcg33(vec3(ip, 0));
+    // get mat
+    float type = MAT_PBR;
+    vec3 albedo = vec3(1);
+    float roughness = 0.5;
+    float metallic = 0.5;
+    // ポイントライトの色
+    vec3 plcol = (step(hash.y, 0.9) * 0.8 + 0.2) * mix(k2000, k12000, hash.x);
+    if(MatID == 0)
+    {
+        // Mat:Concrete
+        vec3 fbm = fbm32(tuv * 3.0 * vec2(3, 1));// gravity ydown
+        vec3 fbm2 = fbm32(tuv * 96.0 * vec2(2, 1));
+        albedo = vec3(saturate(1.3 * mix(0.6, 1.0, fbm.y) * mix(0.8, 1.0, pow(fbm2.x, 3.0))));
+        roughness = mix(0.5, 1.0, pow(fbm.y, 3.0));
+        metallic = 0.01;
+        N = normalize(N + (fbm * 2.0 - 1.0) * 0.05);
+    }
+    else if(MatID == 1)
+    {
+        // Mat:カーテン
+        float h = hash.x;
+        albedo = (h < 0.7 ? vec3(0.8, 0.7, 0.6) : (h < 0.8 ? vec3(0.8, 0.2, 0.2) : (h < 0.9 ? vec3(0.8, 0.6, 0.3) : vec3(0.5, 0.7, 0.8))));
+        roughness = 0.99;
+        metallic = 0.01;
+    }
+    else
+    {
+        // Mat:ライト
+        type = MAT_UNLIT;
+        albedo = plcol;
+    }
+    // avoid self-intersection
+    P += N * DistMin * 2.0;
+    // directional shadow
+    vec3 _rp;
+    vec2 sh = march(DirectionalLight, P, _rp);
+    float visible = sh.y;
+    // primary shading
+    vec3 shaded = vec3(0);
+    // directional light
+    shaded += visible * Microfacet_BRDF(albedo, metallic, roughness, DirectionalLight, -rd, N) * k12000;
+    // point light
+    vec3 L = (isfloorB ? vec3(-RoomSize * 0.5 + 0.1, 2.9) : vec3(sign(hash.x - 0.5), -0.06, 0)) - RP;
+    float l = length(L);
+    L /= l;
+    float pd = max(0.0, l);
+    vec3 lcol = plcol * pow(1.0 / (1.0 + pd), 2.0);
+    shaded += Microfacet_BRDF(albedo, metallic, roughness, L, -rd, N) * lcol;
+    // ao
+    shaded *= sqrt(saturate(sdf(P + N * 0.05) / 0.05));
+    shaded =  mix(shaded, albedo, type);
+    // sky
+    vec4 traced = mix(vec4(sky(rd), LenMax), vec4(shaded, depth), hit.x);
 
-float getcoc(float d, float focalPlane)
-{
-    // focalLength:焦点距離
-    // focalPlane:ピントを合わせたいカメラまでの距離
-    const float focalLength = 0.3;
-    // const float focalLength = 0.35;
-    const float cocScale = 10.0;
-    // 開口径
-    float aperture = min(1.0, focalPlane * focalPlane);
-    float ca = aperture * focalLength * (focalPlane - d);
-    float cb = abs(d * (focalPlane - focalLength)) + 1e-9;
-    float c = abs(ca / cb) * cocScale;
-    return c;
-}
-
-vec3 acesFilm(vec3 x)
-{
+    // 
+    // .%%%%%....%%%%....%%%%...%%%%%%..%%%%%...%%%%%....%%%%....%%%%...%%%%%%...%%%%....%%%%..
+    // .%%..%%..%%..%%..%%........%%....%%..%%..%%..%%..%%..%%..%%..%%..%%......%%......%%.....
+    // .%%%%%...%%..%%...%%%%.....%%....%%%%%...%%%%%...%%..%%..%%......%%%%.....%%%%....%%%%..
+    // .%%......%%..%%......%%....%%....%%......%%..%%..%%..%%..%%..%%..%%..........%%......%%.
+    // .%%.......%%%%....%%%%.....%%....%%......%%..%%...%%%%....%%%%...%%%%%%...%%%%....%%%%..
+    // ........................................................................................
+    // 
+    // Postprocess
+    vec3 col = traced.rgb;
+    // noise乗せた方が雰囲気いいかも
+    col += h3 * 0.03;
+    // 嘘 Gamma Correction
+    col = pow(col, vec3(0.8));
+    // aces
     const float a = 2.51;
     const float b = 0.03;
     const float c = 2.43;
     const float d = 0.59;
     const float e = 0.14;
-    return (x * (a * x + b)) / (x * (c * x + d) + e);
-}
-
-vec3 postprocess(vec3 col, vec3 seed)
-{
-    // noise乗せた方が雰囲気いいか？
-    col += pcg33(seed) * 0.05;
-    // 嘘 Gamma Correction
-    col = pow(col, vec3(0.8));
-    // col = pow(col, vec3(0.4545));
-    col = acesFilm(col);
+    col = (col * (a * col + b)) / (col * (c * col + d) + e);
     // カラグレ
-    #define COG(_s,_b) col._s = smoothstep(0.0-(_b)*0.5,1.0+(_b)*0.5,col._s)
-    COG(r, 0.05);
-    // COG(g, -0.1);
-    // COG(b, 0.05);
+    col.r = smoothstep(-0.05, 1.05, col.r);
 
-    float lt = 0.0;
+    // トランジション
+    lt = 0.0;
     if(tl(0.0, 10.0, lt))
     {
         // 開幕
@@ -674,69 +663,10 @@ vec3 postprocess(vec3 col, vec3 seed)
     {
         col *= 0.0;
     }
+    traced.rgb = col;
 
-    return col;
-}
-
-void main()
-{
-    // Set Time
-    Time = time;
-    // Get UVs
-    vec2 uv = FC / RES;
-    // TAA
-    uv += (pcg33(vec3(FC, Time)).xy - 0.5) * 0.25 / RES;
-    vec2 suv = (uv - 0.5) * 2.0 * ASP;
-
-    // Camera
-    vec3 ro, rd, dir;
-    getRORD(ro, rd, dir, suv);
-
-    // Trace
-    vec4 traced = tracer(rd, ro);
-    // Color Grading
-    traced.rgb = postprocess(traced.rgb, vec3(-Time, 4.2 * uv));
     // TAA&MotionBlur
     const float ema = 0.5;
     vec4 back = texture(backBuffer0, uv);
     outColor0 = mix(traced, back, ema);
-
-    /*
-    outColor1 = max(vec4(0), traced);
-
-    // DOF&Bloom
-    vec4 col = vec4(0);
-    float focalPlane = textureLod(backBuffer1, FocalUV, 3.0).a;
-    vec4 uvtex = texture(backBuffer1, uv);
-    float depth = uvtex.a;
-    float coc = getcoc(depth, focalPlane);
-    const int N = 64;
-    const float SN = sqrt(float(N));
-
-    for(int i = 0; i < N; i++)
-    {
-        float fi = float(i);
-        float r = coc * sqrt(fi) / SN;
-        float th = fi * GOLD;
-        vec2 suv = uv + orbit(th) * r / resolution;
-
-        vec4 stex = texture(backBuffer1, suv);
-        float sd = stex.a;
-
-        if(sd > 0.0)
-        {
-            float scoc = getcoc(sd, focalPlane);
-            float w = max(1e-3, scoc);
-            col += vec4(stex.rgb, 1) * w;
-        }
-    }
-    col.rgb /= col.a;
-
-    // TAA&MotionBlur
-    const float ema = 0.5;
-    vec4 back = texture(backBuffer0, uv);
-    vec3 res = mix(col.rgb, back.rgb, ema);
-    res = max(vec3(0), res);
-    outColor0 = vec4(res, 1);
-    */
 }
